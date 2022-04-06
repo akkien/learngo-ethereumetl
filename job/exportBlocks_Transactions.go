5533e0bc98ef141f8f5349fd49994172e7ea4d47
Le Trung Kien <akkien99@gmail.com>
2022-04-06 14:46:19 +0700

    replace redis by chan

--- job/exportBlocks_Transactions.go
+++ job/exportBlocks_Transactions.go
@@ -10,7 +10,6 @@ import (
 	"github.com/akkien/ethereumetl/rpc"
 	"github.com/akkien/ethereumetl/util"
 	"github.com/gammazero/workerpool"
-	"github.com/go-redis/redis"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -23,75 +22,77 @@ func ParseBlocksAndTransactions(
 	pg *sqlx.DB,
 	batchSize int,
 	maxWorkers int,
-	redisCli *redis.Client,
-	partitionTxsKey string,
-) {
-	wp := workerpool.New(maxWorkers)
-	t1 := time.Now()
-	blockBatchs := util.GeneratePatitions(startBlock, endBlock, batchSize)
+) <-chan []string {
+	txsHashChan := make(chan []string)
 
-	for index, blockBatch := range blockBatchs {
-		start, end := blockBatch[0], blockBatch[1]
-		index := index
-		wp.Submit(func() {
-			// Export
-			blockRange := util.MakeRange(start, end)
-			blockReq, err := rpc.GetBlockRequest(blockRange, true)
-			if err != nil {
-				fmt.Println("Error generate block request", err)
-			}
-			response, err := rpc.Call(providerURI, blockReq)
-			if err != nil {
-				fmt.Println("Error call block rpc", err)
-			}
+	go func() {
+		defer close(txsHashChan)
 
-			t2 := time.Now()
-			fmt.Println(index, "ExportTime", t2.Sub(t1))
+		wp := workerpool.New(maxWorkers)
+		t1 := time.Now()
+		blockBatchs := util.GeneratePatitions(startBlock, endBlock, batchSize)
 
-			// Transfer
-			var blockRes []model.BlockRPCResponse
-			err = json.Unmarshal(response, &blockRes)
-			if err != nil {
-				fmt.Println("Error parse blocks result")
-			}
-
-			blocks, txs := model.RPCResponseToBlock(&blockRes)
-
-			t3 := time.Now()
-			fmt.Println(index, "transferTime", t3.Sub(t1))
-
-			// Load
-			blockQuery, blockValues := db.GetInsertParamsBlock(blocks)
-			res, err := pg.Exec(blockQuery, blockValues...)
-			if err != nil {
-				fmt.Println("Error insert block", err)
-			}
-			fmt.Println(index, "Inserted Blocks:", res)
-
-			if len(txs) > 0 {
-				txQuery, txValues := db.GetInsertParamsTransaction(txs)
-
-				res, err = pg.Exec(txQuery, txValues...)
+		for index, blockBatch := range blockBatchs {
+			start, end := blockBatch[0], blockBatch[1]
+			index := index
+			wp.Submit(func() {
+				// Export
+				blockRange := util.MakeRange(start, end)
+				blockReq, err := rpc.GetBlockRequest(blockRange, true)
 				if err != nil {
-					fmt.Println("Error insert txs:", err)
+					fmt.Println("Error generate block request", err)
 				}
-				fmt.Println(index, "Inserted Transactions:", res)
+				response, err := rpc.Call(providerURI, blockReq)
+				if err != nil {
+					fmt.Println("Error call block rpc", err)
+				}
 
-				// Push txs hash to redis
-				txsHash := make([]interface{}, len(txs))
-				for index, tx := range txs {
-					txsHash[index] = tx.Hash
+				t2 := time.Now()
+				fmt.Println(index, "ExportTime", t2.Sub(t1))
+
+				// Transfer
+				var blockRes []model.BlockRPCResponse
+				err = json.Unmarshal(response, &blockRes)
+				if err != nil {
+					fmt.Println("Error parse blocks result")
 				}
+
+				blocks, txs := model.RPCResponseToBlock(&blockRes)
+
+				t3 := time.Now()
+				fmt.Println(index, "transferTime", t3.Sub(t1))
+
+				// Load
+				blockQuery, blockValues := db.GetInsertParamsBlock(blocks)
+				res, err := pg.Exec(blockQuery, blockValues...)
+				if err != nil {
+					fmt.Println("Error insert block", err)
+				}
+				fmt.Println(index, "Inserted Blocks:", res)
+
 				if len(txs) > 0 {
-					redisCli.RPush(partitionTxsKey, txsHash...)
+					txQuery, txValues := db.GetInsertParamsTransaction(txs)
+
+					res, err = pg.Exec(txQuery, txValues...)
+					if err != nil {
+						fmt.Println("Error insert txs:", err)
+					}
+					fmt.Println(index, "Inserted Transactions:", res)
+
+					// Push txs hash to parent routine
+					txsHash := make([]string, len(txs))
+					for index, tx := range txs {
+						txsHash[index] = tx.Hash
+					}
+					txsHashChan <- txsHash
 				}
-			}
 
-			time.Sleep(1 * time.Second)
-			t5 := time.Now()
-			fmt.Println(index, "exportTxTime", t5.Sub(t1))
-		})
-	}
-	wp.StopWait()
-
+				time.Sleep(1 * time.Second)
+				t5 := time.Now()
+				fmt.Println(index, "exportTxTime", t5.Sub(t1))
+			})
+		}
+		wp.StopWait()
+	}()
+	return txsHashChan
 }
--- job/job.go
+++ job/job.go
@@ -3,10 +3,8 @@ package job
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/akkien/ethereumetl/util"
-	"github.com/go-redis/redis"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -27,31 +25,21 @@ func ExportAll(
 	}
 	db.SetMaxOpenConns(maxWorkers + 1)
 
-	redisCli := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-	_, err = redisCli.Ping().Result()
-	if err != nil {
-		panic(err)
-	}
 	fmt.Println("Database Connected")
 
 	/** Parse Blocks & Transactions **/
 	blockPartitions := util.GeneratePatitions(startBlock, endBlock, paritionBatchSize)
-	partitionTxsKey := strconv.Itoa(startBlock) + "_" + strconv.Itoa(endBlock) + "-TXS"
+	var partitionTxsHash []string
+
 	for _, partition := range blockPartitions {
 		fmt.Println("Partition", partition)
-		ParseBlocksAndTransactions(partition[0], partition[1], providerURI, db, batchSize, maxWorkers, redisCli, partitionTxsKey)
+		txsHashChan := ParseBlocksAndTransactions(partition[0], partition[1], providerURI, db, batchSize, maxWorkers)
+
+		for txsHash := range txsHashChan {
+			/** Parse Receipts & Logs **/
+			partitionTxsHash = append(partitionTxsHash, txsHash...)
+		}
 	}
 
-	/** Parse Receipts & Logs **/
-	txsHash, err := redisCli.LRange(partitionTxsKey, 0, -1).Result()
-	if err != nil {
-		panic(err)
-	}
-	redisCli.Del(partitionTxsKey)
-	ParseReceiptsAndLogs(txsHash, providerURI, db, batchSize, maxWorkers)
-
+	ParseReceiptsAndLogs(partitionTxsHash, providerURI, db, batchSize, maxWorkers)
 }
